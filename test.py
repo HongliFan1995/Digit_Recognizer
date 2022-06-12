28e49048cabf800212e3f9bdf2d7b6336b0e3b26
Hongli Fan <honglifan1995@gmail.com>
2022-06-12 19:52:30 -0400

    remove testpy

--- test.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-from tensorflow.keras import datasets, layers, models
-import matplotlib.pyplot as plt
-
-(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
-print(train_images.shape)
-print(train_labels.shape)
-print(train_labels[0:4])
